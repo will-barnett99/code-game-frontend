e3d51bfece76060d69a2f8e008f4064cc5f95832
DarkoGjichev <darkogicev111@gmail.com>
2025-08-27 09:07:02 +0100

    add init kata component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'
-import { Route, Routes } from 'react-router'
-import Landing from './components/Landing'
-import Header from './components/Header'
-import Authentication from './components/Authentication'
-
-
-
+import { Route, Routes } from "react-router";
+import Landing from "./components/Landing";
+import Header from "./components/Header";
+import Authentication from "./components/Authentication";
+import Kata from "./components/Kata";
 
 function App() {
-
   return (
     <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Landing />}/>
-      <Route path="/authentication" element={<Authentication />}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/authentication" element={<Authentication />} />
+        <Route path="/kata" element={<Kata />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
