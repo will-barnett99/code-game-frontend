de6363917ef3b1eb736fd75b832c41cd0d4f8938
On-KeiShe <on-kei_she@hotmail.co.uk>
2025-08-27 10:03:27 +0100

    Create KataProfile Component

---
+++ src/components/KataProfile.jsx
@@ -0,0 +1,14 @@
+function KataProfile (){
+
+  return (
+  <>
+  <img src="./assets/360_F_633547842_AugYzexTpMJ9z1YcpTKUBoqBF0CUCk10.jpg"/>
+  <p>Name</p>
+  <p>Level: Expert</p>
+  <p>#124</p>
+  </>
+  )
+
+}
+
+export default KataProfile
