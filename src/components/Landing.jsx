b07e706d69de10a6a8c3bcc226aeb3b09f9cba82
DarkoGjichev <darkogicev111@gmail.com>
2025-08-27 15:28:06 +0100

    add link on Landing component for Onboarding component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,14 @@
-function Landing() {
+import { Link } from "react-router";
 
+function Landing() {
+  return (
+    <>
+      <h1>Welcome to our coding game</h1>
+      <Link>
+        <button>Start</button>
+      </Link>
+    </>
+  );
 }
 
 export default Landing;
