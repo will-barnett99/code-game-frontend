522aa9d10a429fae85e4deca5b3c889bc265b8db
DarkoGjichev <darkogicev111@gmail.com>
2025-08-27 15:48:10 +0100

    add onboarding component

---
+++ src/components/Onboarding.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router";
+
+function Onboarding() {
+  return (
+    <>
+      <h3>WELCOME TO KataQuest</h3>
+      <p>
+        Want to test your coding knowledge? whether a complete beginner, an
+        expert or somewhere in between - traverse the world of KataQuest by
+        completing katas!
+      </p>
+      <Link to={`/kata-gallery`}>
+        <button>Start</button>
+      </Link>
+    </>
+  );
+}
+
+export default Onboarding;
