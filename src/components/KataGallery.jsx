e2726b2f97a67eec243bdb837032cb828929616c
DarkoGjichev <darkogicev111@gmail.com>
2025-08-27 15:49:18 +0100

    add links on every kata card to access the code editor

--- src/components/KataGallery.jsx
+++ src/components/KataGallery.jsx
@@ -2,12 +2,28 @@ import KataCard from "./KataCard";
 
 function KataGallery() {
   const katas = [
-    { id: 1, title: "FizzBuzz", topic:"Strings", difficulty: "Beginner" , description: "Classic loop challenge."},
-    { id: 2, title: "Palindrome Checker", topic:"Strings", difficulty: "Intermediate", description: "Check if a word reads the same backwards."},
-    { id: 3, title: "Roman Numerals", topic:"Numbers", difficulty: "Advanced", description: "Convert numbers to Roman numerals."},
+    {
+      id: 1,
+      title: "FizzBuzz",
+      topic: "Strings",
+      difficulty: "Beginner",
+      description: "Classic loop challenge.",
+    },
+    {
+      id: 2,
+      title: "Palindrome Checker",
+      topic: "Strings",
+      difficulty: "Intermediate",
+      description: "Check if a word reads the same backwards.",
+    },
+    {
+      id: 3,
+      title: "Roman Numerals",
+      topic: "Numbers",
+      difficulty: "Advanced",
+      description: "Convert numbers to Roman numerals.",
+    },
   ];
-
-
 
   return (
     <>
@@ -19,11 +35,10 @@ function KataGallery() {
           topic={kata.topic}
           difficulty={kata.difficulty}
           description={kata.description}
-
         />
       ))}
     </>
   );
 }
 
-export default KataGallery
+export default KataGallery;
