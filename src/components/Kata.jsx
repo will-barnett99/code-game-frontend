156a387a9f2bf1af22944c298b28b04da7858c87
DarkoGjichev <darkogicev111@gmail.com>
2025-08-27 09:33:59 +0100

    setup init codeMirror component inside of kata component

--- src/components/Kata.jsx
+++ src/components/Kata.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
 
 function Kata() {
   const [input, setInput] = useState("");
-  return <></>;
+
+  return (
+    <>
+      <CodeMirror height="300px" extensions={[javascript()]} />
+    </>
+  );
 }
 export default Kata;
