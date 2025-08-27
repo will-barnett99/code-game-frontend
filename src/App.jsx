5eb4d16948b42a6771bcb2a9609f5864ca64f55a
DarkoGjichev <darkogicev111@gmail.com>
2025-08-27 15:48:53 +0100

    links landing component with onboarding component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Authentication from "./components/Authentication";
 import Kata from "./components/Kata";
 import KataGallery from "./components/KataGallery";
+import Onboarding from "./components/Onboarding";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/onboarding" element={<Onboarding />} />
         <Route path="/authentication" element={<Authentication />} />
         <Route path="/kata" element={<Kata />} />
         <Route path="/kata-gallery" element={<KataGallery />} />
