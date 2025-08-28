882f39ad1a1a104525eb4f03df24d321f6954a76
DarkoGjichev <darkogicev111@gmail.com>
2025-08-28 14:51:00 +0100

    attached background image to root so it travels with diferent paths

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,20 +1,22 @@
 import { Link } from "react-router";
-import bgClassic from "/assets/Background-Image.svg";
-
 function Landing() {
   return (
     <>
-      <section
-        className="min-h-screen  bg-cover bg-center flex flex-col items-center justify-start text-white"
-        style={{ backgroundImage: `url(${bgClassic})` }}
-      >
-        <h1 className="font-pixelify text-9xl font-bold mt-70 mb-6">
+      <section className="h-[calc(100%-60px)] flex flex-col gap-16 content-center justify-center text-white text-center">
+        <h1 className="font-pixelify text-9xl font-bold">
           Welcome to our coding game
         </h1>
-        
-        <Link to={`/onboarding`}>
-          <button className="font-pixelify text-6xl font-bold mt-50 mb-6 w-100 h-100 bg-contain bg-no-repeat bg-center text-white
-             flex items-center justify-center" style={{ backgroundImage: "url('/assets/game-controller-joystick-button.png')" }}>Start</button>
+
+        <Link to={`/onboarding`} className="">
+          <button
+            className="font-pixelify text-6xl font-bold w-[372px] h-[160px] bg-center bg-cover cursor-pointer"
+            style={{
+              backgroundImage:
+                "url('/assets/game-controller-joystick-button.png')",
+            }}
+          >
+            Start
+          </button>
         </Link>
       </section>
     </>
