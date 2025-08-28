3488f8c4b6d10a5eff464babc408352c9f64c7f2
DarkoGjichev <darkogicev111@gmail.com>
2025-08-28 13:28:48 +0100

    add icons on cards, small styling, padding, margins and display settings

--- src/components/KataCard.jsx
+++ src/components/KataCard.jsx
@@ -1,16 +1,35 @@
 import { Link } from "react-router";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBolt,
+  faDumbbell,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
 function KataCard({ katas }) {
   return (
     <>
       {katas.map((kata) => (
-        <section key={kata.id}>
-          <p>Kata Title: {kata.title}</p>
-          <p>Topic: {kata.topic}</p>
-          <p>Difficulty: {kata.difficulty}</p>
-          <p>Description: {kata.description}</p>
-          <Link to={`/kata`}>
-            <button>try this kata</button>
+        <section
+          key={kata.id}
+          className="flex flex-col gap-2 border border-gray-300 rounded-xl p-2 mb-4 bg-gray-50"
+        >
+          <header className="flex items-center justify-between flex-wrap gap-4">
+            <h4 className="font-semibold">{kata.title}</h4>
+            <ul className="flex gap-4">
+              <li className="text-yellow-700">
+                <FontAwesomeIcon icon={faBolt} /> {kata.topic}
+              </li>
+              <li className="text-green-700">
+                <FontAwesomeIcon icon={faDumbbell} className="rotate-45" />{" "}
+                {kata.difficulty}
+              </li>
+            </ul>
+          </header>
+          <p>{kata.description}</p>
+          <Link to={`/kata`} className="ml-auto mt-8">
+            <button className="border border-gray-300 px-4 py-2 rounded cursor-pointer">
+              try this kata <FontAwesomeIcon icon={faArrowRight} />
+            </button>
           </Link>
         </section>
       ))}
