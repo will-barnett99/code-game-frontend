6e7a66d152dcd3605791c73a19c5e5b23bff3b81
will-barnett99 <willbarnett99@hotmail.com>
2025-08-28 11:41:32 +0100

    hardcoding kata textbox content and formatting basic tailwindCSS

--- src/components/textbox-comps/kata-description.jsx
+++ src/components/textbox-comps/kata-description.jsx
@@ -2,7 +2,7 @@ function KataDescription () {
     return (
        <section>
             <h3>Breakdown</h3>
-            <p>I AM THE KATA BREAKDOWN</p>
+            <p>Return the Sum! This function should take two integers (a,b) and return the sum of both.</p>
         </section>
     )
 }
