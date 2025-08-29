11dbc0534e0351bda9dae5293872d98bc33ddc36
DarkoGjichev <darkogicev111@gmail.com>
2025-08-29 08:35:46 +0100

    init setup for kata execution

--- src/components/Kata.jsx
+++ src/components/Kata.jsx
@@ -15,27 +15,45 @@ function Kata() {
 };`,
     tests: ["assertEqual(sum(1,2), 3)", "assertEqual(sum(-1,5), 4)"],
   };
-
   const [input, setInput] = useState(kata.starterCode);
+  const [output, setOutput] = useState("");
 
   function assertEqual(actual, expected) {
     if (actual !== expected) {
       throw new Error(`Not quite right`);
     }
   }
+  const handleRun = () => {
+    try {
+      const functionBody = `${input}${kata.tests.join(";\n")}`;
+      const userFunction = new Function("assertEqual", functionBody);
+      userFunction(assertEqual);
+      setOutput("Well done");
+    } catch (err) {
+      setOutput(err.message);
+    }
+  };
+
+  const handleReset = () => {
+    setInput(kata.starterCode);
+    setOutput("");
+  };
 
   return (
-    <>
-      <KataProfile />
-      <Textbox />
+    <div>
+      <h1>{kata.title}</h1>
+      <p>{kata.prompt}</p>
       <CodeMirror
         value={input}
         height="300px"
         extensions={[javascript()]}
         onChange={(value) => setInput(value)}
-      />
-      <ButtonsContainer initCode={kata.starterCode} setInput={setInput} />
-    </>
+      >
+        <button onClick={handleRun}>Run</button>
+        <button onClick={handleReset}>Reset</button>
+      </CodeMirror>
+      <pre>{output}</pre>
+    </div>
   );
 }
 export default Kata;
