4f385544c4b2e3cd42e18e8dd3cc1dd16fff266b
DarkoGjichev <darkogicev111@gmail.com>
2025-09-02 14:32:17 +0100

    add clerkProvider to App

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { ClerkProvider } from "@clerk/clerk-react";
+
 const PUBLISHABLE_KEY =
   "pk_test_ZmluZS1wYXJyb3QtNjAuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
 if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </BrowserRouter>
 );
