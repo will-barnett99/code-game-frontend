0d4bfb3785b845fb1afd431d30d8b5fd12f24062
DarkoGjichev <darkogicev111@gmail.com>
2025-09-02 15:34:42 +0100

    remove custom authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router";
 import Landing from "./components/Landing";
 import Header from "./components/Header";
-import Authentication from "./components/Authentication";
 import Kata from "./components/Kata";
 import KataGallery from "./components/KataGallery";
 import Onboarding from "./components/Onboarding";
@@ -20,7 +19,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/authentication" element={<Authentication />} />
         <Route path="/kata-gallery/:kata_id" element={<Kata />} />
         <Route path="/kata-gallery" element={<KataGallery />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
