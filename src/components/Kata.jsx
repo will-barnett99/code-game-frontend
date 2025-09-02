8e2b976e66832acadb8a345bdf3d57b8643f41ef
DarkoGjichev <darkogicev111@gmail.com>
2025-09-02 11:14:55 +0100

    add logic so react doesn't break on failed api call

--- src/components/Kata.jsx
+++ src/components/Kata.jsx
@@ -15,7 +15,6 @@ function Kata() {
 
   useEffect(() => {
     getSingleKata(kata_id).then(({ kata }) => {
-      console.log(kata);
       setKata(kata);
       setInput(kata.initial_code);
     });
@@ -36,30 +35,32 @@ function Kata() {
   const handleHint = () => {
     setOutput("Try returning the sum of a and b");
   };
-  return (
-    <main className="w-[1024px] h-[560px] mx-auto mt-24 p-8 border-8 border-orange-500 bg-yellow-400 text-orange-700 [box-shadow:8px_8px_0_#000000]">
-      <section className="flex gap-8">
-        <section className="w-1/3">
-          <KataProfile />
-          <Textbox title={kata.title} description={kata.description} />
-        </section>
+  if (kata) {
+    return (
+      <main className="w-[1024px] h-[560px] mx-auto mt-24 p-8 border-8 border-orange-500 bg-yellow-400 text-orange-700 [box-shadow:8px_8px_0_#000000]">
+        <section className="flex gap-8">
+          <section className="w-1/3">
+            <KataProfile />
+            <Textbox title={kata.title} description={kata.description} />
+          </section>
 
-        <section className="w-2/3 flex flex-col gap-4">
-          <CodeMirror
-            value={input}
-            height="250px"
-            extensions={[javascript()]}
-            onChange={(value) => setInput(value)}
-          />
-          <ButtonsContainer
-            handleRun={handleRun}
-            handleReset={handleReset}
-            handleHint={handleHint}
-          />
-          <pre className="mt-auto text-right text-xl font-bold">{output}</pre>
+          <section className="w-2/3 flex flex-col gap-4">
+            <CodeMirror
+              value={input}
+              height="250px"
+              extensions={[javascript()]}
+              onChange={(value) => setInput(value)}
+            />
+            <ButtonsContainer
+              handleRun={handleRun}
+              handleReset={handleReset}
+              handleHint={handleHint}
+            />
+            <pre className="mt-auto text-right text-xl font-bold">{output}</pre>
+          </section>
         </section>
-      </section>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 export default Kata;
