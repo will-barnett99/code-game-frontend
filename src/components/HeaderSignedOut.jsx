2c89571b16614c58b68fcd96ee7eb87ceded574f
DarkoGjichev <darkogicev111@gmail.com>
2025-09-03 13:46:35 +0100

    add sign in button on header

--- src/components/HeaderSignedOut.jsx
+++ src/components/HeaderSignedOut.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
+import { SignInButton } from "@clerk/clerk-react";
 
-function HeaderSignedOut () {
+function HeaderSignedOut() {
   return (
     <header className="flex items-center justify-start px-4">
       <h1 className="font-pixelify text-4xl font-bold px-4 pt-2 text-auto ">
@@ -8,9 +9,7 @@ function HeaderSignedOut () {
       </h1>
 
       <nav className="font-pixelify text-xl font-bold ml-auto mr-40 mt-5 text-auto ">
-        <Link to="/" className="px-4">
-          Home
-        </Link>
+        <SignInButton />
       </nav>
     </header>
   );
