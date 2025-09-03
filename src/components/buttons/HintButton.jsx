0d68795432628bbd6ce0d057c4dd8e88232048aa
DarkoGjichev <darkogicev111@gmail.com>
2025-09-03 08:38:25 +0100

    replace class with className

--- src/components/buttons/HintButton.jsx
+++ src/components/buttons/HintButton.jsx
@@ -2,7 +2,7 @@ function HintButton({ handleHint }) {
   return (
     <button
       onClick={handleHint}
-      class="w-20 bg-orange-500 text-white hover:bg-orange-600 hover:[box-shadow:4px_4px_0_#000000] cursor-pointer transition-transform duration-200 ease-out hover:-translate-y-0.5 hover:-translate-x-0.5"
+      className="w-20 bg-orange-500 text-white hover:bg-orange-600 hover:[box-shadow:4px_4px_0_#000000] cursor-pointer transition-transform duration-200 ease-out hover:-translate-y-0.5 hover:-translate-x-0.5"
     >
       ? Hint ?
     </button>
