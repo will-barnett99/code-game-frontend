82959e2d146a430d3a8620bdfdae3e045e2ca9af
DarkoGjichev <darkogicev111@gmail.com>
2025-09-04 12:02:12 +0100

    pass tags from parent and conditional logic if tags are undefined

--- src/components/Textbox.jsx
+++ src/components/Textbox.jsx
@@ -1,15 +1,16 @@
 import KataDescription from "./textbox-comps/kata-description";
 import RelativeNotes from "./textbox-comps/relative-notes";
 
-function Textbox({ title, description, kata_id }) {
+function Textbox({ title, description, kata_id, tags, note }) {
   return (
     <article>
       <KataDescription
         title={title}
         description={description}
         kata_id={kata_id}
+        tags={tags}
       />
-      <RelativeNotes />
+      <RelativeNotes note={note} />
     </article>
   );
 }
