6d8b040b975a5ef9fadaac5f92b2bd6440e9565f
DarkoGjichev <darkogicev111@gmail.com>
2025-09-04 10:40:31 +0100

    add responsive breakpoints for code-editor component

--- src/components/Kata.jsx
+++ src/components/Kata.jsx
@@ -52,32 +52,35 @@ function Kata() {
   };
 
   return (
-    <main className="w-[1024px] h-[560px] mx-auto mt-24 p-8 border-8 border-orange-500 bg-yellow-400 text-orange-700 [box-shadow:8px_8px_0_#000000]">
-      <section className="flex gap-8 h-[100%]">
-        <section className="w-1/3">
-          <KataProfile />
-          <Textbox
-            title={kata.title}
-            description={kata.description}
-            kata_id={kata_id}
-          />
-        </section>
-
-        <section className="w-2/3 flex flex-col gap-4">
-          <CodeMirror
-            value={input}
-            height="250px"
-            extensions={[javascript()]}
-            onChange={(value) => setInput(value)}
-          />
-          <ButtonsContainer
-            handleRun={handleRun}
-            handleReset={handleReset}
-            handleHint={handleHint}
-          />
-          <pre className="mt-auto text-right text-xl font-bold">{output}</pre>
-          {pass ? <NextKata kata_id={kata_id} /> : null}
-        </section>
+    <main
+      className="w-100% lg:h-[560px] max-w-[1024px] flex flex-col lg:flex-row mx-4 mt-8 lg:mt-24 p-4 gap-4 lg:gap-8 lg:mx-auto
+                 border-8 border-orange-500 bg-yellow-400 text-orange-700 [box-shadow:8px_8px_0_#000000]"
+    >
+      <section className="lg:w-1/3 ">
+        <KataProfile />
+        <Textbox
+          title={kata.title}
+          description={kata.description}
+          kata_id={kata_id}
+        />
+      </section>
+      <section className="lg:w-2/3 flex flex-col gap-2 lg:gap-4">
+        <CodeMirror
+          value={input}
+          width="100%"
+          height="250px"
+          extensions={[javascript()]}
+          onChange={(value) => setInput(value)}
+        />
+        <ButtonsContainer
+          handleRun={handleRun}
+          handleReset={handleReset}
+          handleHint={handleHint}
+        />
+        <p className="lg:mt-auto text-center lg:text-right lg:font-bold">
+          {output}
+        </p>
+        {pass ? <NextKata kata_id={kata_id} /> : null}
       </section>
     </main>
   );
