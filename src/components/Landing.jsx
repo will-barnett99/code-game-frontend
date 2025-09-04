5dd686e519330ce8d21c4bf8e47c918bdc5f41c8
DarkoGjichev <darkogicev111@gmail.com>
2025-09-04 08:34:42 +0100

    add responsive breakpoints fo text and padding

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,24 +5,23 @@ import { faGamepad } from "@fortawesome/free-solid-svg-icons";
 function Landing() {
   return (
     <>
-      <section className=" flex flex-col gap-16 content-center justify-center text-white text-center">
-        <h1 className="font-pixelify text-9xl font-bold pt-48">
+      <section className="w-[100vw] h-[calc(100vh-48px)] flex flex-col gap-4  md:gap-8 lg:gap-16 items-center justify-center lg:justify-start lg:pt-48 text-center font-pixelify text-white">
+        <h1 className="max-w-[1024px] text-5xl md:text-7xl lg:text-9xl font-bold">
           Welcome to our coding game
         </h1>
 
-        <Link
-          to={`/onboarding`}
-          className="font-pixelify font-bold cursor-pointer text-center pt-60"
-        >
-          <FontAwesomeIcon
-            icon={faGamepad}
-            className="text-6xl text-gray-500"
-          />
-          <button className="text-6xl px-4">Start</button>
-          <FontAwesomeIcon
-            icon={faGamepad}
-            className="text-6xl text-gray-500"
-          />
+        <Link to={`/onboarding`}>
+          <button className="flex items-center gap-2 mx-auto p-4 text-3xl md:text-5xl lg:text-7xl cursor-pointer font-bold">
+            <FontAwesomeIcon
+              icon={faGamepad}
+              className="text-gray-500 text-3xl lg:text-5xl"
+            />{" "}
+            Start
+            <FontAwesomeIcon
+              icon={faGamepad}
+              className="text-gray-500 text-3xl lg:text-5xl"
+            />
+          </button>
         </Link>
       </section>
     </>
