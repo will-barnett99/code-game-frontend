f95b957f75d7e6dffc3307c1cbd321cc9f6a371b
DarkoGjichev <darkogicev111@gmail.com>
2025-09-04 12:01:26 +0100

    remove state, pass state from parent instead also conditional logic for not breaking the code in case of unsusceful get func

--- src/components/KataTags.jsx
+++ src/components/KataTags.jsx
@@ -1,20 +1,11 @@
-import { useEffect, useState } from "react";
-import getKataContent from "../api/getKataContent";
-
-function KataTags({ kata_id }) {
-  const [tags, setTags] = useState([]);
-  useEffect(() => {
-    getKataContent(kata_id, "tags").then(({ tags }) => {
-      setTags(tags);
-    });
-  }, [kata_id]);
-  return (
-    <ul className="kata-tags">
+function KataTags({ tags }) {
+  return tags && tags.length > 0 ? (
+    <ul>
       {tags.map((tag) => (
         <li key={tag}>{tag}</li>
       ))}
     </ul>
-  );
+  ) : null;
 }
 
 export default KataTags;
